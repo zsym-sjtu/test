0f16fb2f39cf6df69282f2273216668d06ad0a07
zsym <zsym2019@sjtu.edu.cn>
2023-11-03 14:51:55 +0800

    good

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -11,8 +11,10 @@ class MainTest {
 		Main main = new Main();
 		int result = main.add(1, 1);
 		assertEquals(2, result);
+		result = main.add(1, -1);
+		assertEquals(0, result);
+		result = main.add(1, -101);
+		assertEquals(-100, result);
 	}
 
 }
-
-51rupaweijfoiuasehfouhefouhaoiufeoifjeroiugjeoirjgoi
